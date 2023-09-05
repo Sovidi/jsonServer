44b001d0a6dee4b3ce3e12611cf6c8adac5e8a7a
wjdalswjdals <roaring_frog@zohomail.com>
2023-09-05 12:48:35 +0900

    wsdfsefd

--- index.js
+++ index.js
@@ -10,25 +10,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+const data = {
+    select : function(){
+        return JSON.parse(fs.readFileSync("./test.json"));
+    },
+    insert : function(newObj){
+        const jsonData = data.select();
+        let newData = [...jsonData, {id:jsonData.length+1, ...newObj}];
+        fs.writeFileSync("./test.json", JSON.stringify(newData));
+        return newData;
+    },
+    update : function(){},
+    delete : function(){}
+}
+
+
 app.get('/abc', function (req, res) {
-    const jsonData = fs.readFileSync("./test.json");
-    res.send( JSON.parse(jsonData));
+    // console.log(req.body)
+    // console.log(req.query)
+    // console.log(req.params)
+    res.send(data.select());
 });
 
-// app.get('/abc/:id', function (req, res) {
-//     const jsonData = fs.readFileSync("./test.json");
-//     const data = JSON.parse(jsonData);
-//     const {id} = req.params;
-//     const aaa = data.filter(item => item.id == id)
-//     res.send(aaa);
-// });
+app.delete('/abc/:id', function (req, res) {
+    const jsonData = data.select();
+    const {id} = req.params;
+    const delData = jsonData.filter(item => item.id != id);
+    
+    fs.writeFileSync("./test.json", JSON.stringify(delData))
+    res.send(delData);
+});
 
 app.post('/insert', function (req, res) {
-    console.log(req.body)
-    const jsonData = fs.writeFileSync("./test.json", JSON.stringify(req.body))
-    res.send("성공");
+    // console.log(req.body)
+    // console.log(req.query)
+    // console.log(req.params)
+    // {id:0, ...req.body}
+    res.send( data.insert(req.body) );
 });
 
 
 
-app.listen(3030);
+app.listen(3040);
